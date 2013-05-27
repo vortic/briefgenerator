75d31781781e3b0916f85b1d1c682e27a53ad0d3
Andrew Gambardella <andrewgambardella18902@gmail.com>
2013-05-27 14:59:12 -0700

    opinionsToGraph outputs a d3-friendly graph

--- util/opinionsToGraph.py
+++ util/opinionsToGraph.py
@@ -1,61 +1,63 @@
 import re
 
-def getTitles(filename):
+def getNodes(filename):
     title = False
-    nodeCases = []
+    body = False
+    caseText = ''
+    nodeName = ''
+    nodes = []
     with open(filename) as f:
         lines = f.readlines()
     for line in lines:
         line = line.strip()
         if re.match('[0-9]+ of 100 DOCUMENTS', line): #Matches title of a new brief
+            if nodeName is not '':
+                nodes.append({"name":nodeName,"group":0,"text":caseText})
+            caseText = ''
             title = True
             continue
-        if re.match('PRIOR-HISTORY', line): #Matches end of title
+        if re.match('OPINION', line): #Matches body
             title = False
+            body = True
         if title:
             reg = '\d+ Cal\..*\d+' #Matches the reference to the case (eg 220 Cal.App.2d 1)
             if re.match(reg, line):
                 splitNodeNames = line.split(';')
-                nodeCase = splitNodeNames[0]
-                nodeCases.append(nodeCase)
-    return nodeCases
-
-def getLinks(filename, numDocs):
-    body = False
-    nodeLinks = [0]*numDocs
-    docNum = -1
-    previousSentence = ''
-    with open(filename) as f:
-        lines = f.readlines()
-    for line in lines:
-        line = line.strip()
-        if re.match('[0-9]+ of 100 DOCUMENTS', line): #Matches title of a new brief
-            body = False
-            docNum += 1
-            nodeLinks[docNum] = []
-            continue
-        if re.match('OPINION', line): #Matches body
-            body = True
+                nodeName = splitNodeNames[0]
         if body:
-            reg = '.*([?!\d]*) (\d+ Cal\..* \d+)(;|,| \]).*' #Matches the reference to the case (eg 220 Cal.App.2d 1)
-            result = re.match(reg, line)
-            if result:
-                potentialLink = result.group(2)
-                context = previousSentence
-                if len(potentialLink.split(' ')) == 3:
-                    nodeLinks[docNum].append((potentialLink,context))
-        previousSentence = line
-    return nodeLinks
+            caseText += unicode(line, errors='ignore') + ' '
+    return nodes
 
+def getAllNodesAndLinks(nodes):
+    links = []
+    allNodes = nodes
+    for op,docNum in zip(nodes,range(0,len(nodes))):
+        body = op.get("text")
+        #reg = '(\d+\s+?Cal\..\s?App\s?.*?\s+\d+)[;|,|\s\[|\.]' #Only gets links with "App" in them
+        reg = '(\d+\s+?Cal\.\s?[a-zA-Z0-9\.]*?\s+?\d+)[;|,|\s\[|\.]+?' #Gets all links
+        for result in re.finditer(reg, body):
+            for potentialLink in result.groups():
+                if not 'at' in potentialLink: #Ignoring things like (55 Cal. App. 4th at p. 1065)
+                    foundLink = False
+                    for link in allNodes:
+                        if link.get("name") == potentialLink:
+                            links.append({"source":docNum,\
+                                "target":allNodes.index(link),"value":5})
+                            foundLink = True
+                    if not foundLink:
+                        newNodeName = {"name":potentialLink,"group":9,"text":''}
+                        allNodes.append(newNodeName)
+                        links.append({"source":docNum,\
+                            "target":allNodes.index(newNodeName),"value":5})
+    return (allNodes, links)
 
 if __name__ == "__main__":
-    import argparse
+    import argparse, json, os
     parser = argparse.ArgumentParser(description='Get links from a file.')
     parser.add_argument('f', metavar='filename', action='store', help='File to parse (LexisNexis format)')
     args = parser.parse_args()
-    graph = {}
-    titles = getTitles(args.f)
-    links = getLinks(args.f, len(titles))
-    for node,edges in zip(titles, links):
-        graph[node] = edges
-    print graph
+    nodes = getNodes(args.f)
+    (allNodes, links) = getAllNodesAndLinks(nodes)
+    with open('opinions.json', 'w') as out:
+        out.write(json.dumps({"nodes":allNodes,"links":links}))
+
