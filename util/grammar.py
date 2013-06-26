5b54352bf4feb13637d7500cb849596a281d09b2
Andrew Gambardella <andrewgambardella18902@gmail.com>
2013-06-26 14:15:50 -0700

    Added more to the grammar

--- util/grammar.py
+++ util/grammar.py
@@ -17,15 +17,63 @@ t_VC = r'[rR]eject|[dD]ismiss|[dD]eclare'
 t_F = r'[cC]ourt|[jJ]ury|[cC]ommission'
 
 def p_t(p):
-    't:aplus d'
+    't:aPlus d'
     #p[0] = p[1] + p[3]
 
 def p_a(p):
-    '''a:aplus c
-        |astar c N pplus
+    '''a:aPlus c
+        |aStar c N pPlus
         |c N S
-        |astar S RC c
-        |pplus'''
+        |aStar S RC c
+        |pPlus'''
+    #p[0] = p[1] + p[3]
+
+def p_d(p):
+    'd:RC F dHelper'
+    #p[0] = p[1] + p[3]
+
+def p_p(p):
+    '''p:pVerbP
+        |pArt
+        |p pSup
+        |p pAg
+        |S pSup
+        |S pAg'''
+    #p[0] = p[1] + p[3]
+
+def p_pVerbP(p):
+    'pVerbP:S VP S'
+    #p[0] = p[1] + p[3]
+
+def p_pArt(p):
+    'pArt:S RART S'
+    #p[0] = p[1] + p[3]
+
+def p_pSup(p):
+    '''pSup:RS S
+        |RS pVerbP
+        |RS pArt
+        |RS pSup
+        |RS pAg'''
+    #p[0] = p[1] + p[3]
+
+def p_pAg(p):
+    '''pAg:RA S
+        |RA pVerbP
+        |RA pArt
+        |RA pSup
+        |RA pAG'''
+    #p[0] = p[1] + p[3]
+
+def p_c(p):
+    '''c:RC S
+        |RC c
+        |RC RC pVerbP
+        |RS S
+        |RS c
+        |RS RC pVerbP
+        |sStar VC S
+    '''
     #p[0] = p[1] + p[3]
 
 parser = yacc.yacc()
