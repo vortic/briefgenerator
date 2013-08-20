aa2f201e5f69a024ca9891f544ea0d387e476fe6
Andrew Gambardella <andrewgambardella18902@gmail.com>
2013-08-20 02:39:58 -0700

    Lots of SRL bugfixes

--- util/data.py
+++ util/data.py
@@ -12,7 +12,11 @@ def getHeader():
     return header
 
 def getGoogleURL(caseName):
-    url = 'http://scholar.google.com/scholar?hl=en&q=' + caseName.replace(' ', '+') + '&btnG=&as_sdt=4%2C5'
+    nameAndYear = caseName.split(', ')
+    url = 'http://scholar.google.com/scholar?hl=en&q=' + nameAndYear[0].replace(' ', '+') + '&btnG=&as_sdt=4%2C5'
+    if len(nameAndYear) > 1:
+        url = url + '&as_ylo=' + nameAndYear[1]  + '&as_yhi=' + nameAndYear[1]
+    print url
     header = getHeader()
     req = urllib2.Request(url, None, header)
     html = urllib2.urlopen(req).read()
@@ -49,7 +53,7 @@ def getGoogleCites(caseName):
         if 'and' in title:
             split = re.split('and', title)
             ret = split[0]
-        return ret
+        return ret.strip()
     urlNum = getGoogleURL(caseName)
     url = 'http://scholar.google.com/scholar_case?about=' + urlNum
     header = getHeader()
@@ -75,13 +79,23 @@ def getGoogleCites(caseName):
     return cites, titles
 
 def getGoogleCase(caseName):
+    def removeHeading(raw):
+        headingRemover = re.compile('.*?OPINION(.*)Save\strees\s-\sread', re.DOTALL)
+        for result in re.finditer(headingRemover, raw):
+            for case in result.groups():
+                return case
+        #Failure: just keep Google's stuff in there
+        return raw
     urlNum = getGoogleURL(caseName)
     url = 'http://scholar.google.com/scholar_case?case=' + urlNum
     header = getHeader()
     req = urllib2.Request(url, None, header)
     html = urllib2.urlopen(req).read()
     raw = nltk.clean_html(html)
-    return raw
+    ret = removeHeading(raw)
+    if ret == None:
+        print "Warning: Could not get " + caseName
+    return ret
 
 if __name__ == "__main__":
     cites, titles = getGoogleCites('224 Cal. App. 3d 885')
--- util/linguistics.py
+++ util/linguistics.py
@@ -1,6 +1,7 @@
 import sys, os, re
 import opinionsToGraph as otg
 from subprocess import *
+import time
 import pexpect
 import nltk
 from nltk.tree import *
@@ -43,14 +44,20 @@ def getAction(sentence, noun):
     if draw:
         pTree.draw()
 
-def startProcess(cmdString, log=False):
+def startProcessPexpect(cmdString, log=True):
     cmd = pexpect.spawn(cmdString)
     if log:
-        cmd.logfile = open("tmp/sennalog", "w")
+        cmd.logfile = open("tmp/log.txt", "w")
     return cmd
 
+def start30SecProcess(cmdString):
+    cmd = Popen(cmdString, shell=True)
+    time.sleep(30)
+    cmd.terminate()
+    return None
+
 def startSRL():
-    labeler = startProcess("./lib/ASRL/senna/senna -path lib/ASRL/senna/ -srl -verbose")
+    labeler = startProcessPexpect("./lib/ASRL/senna/senna -path lib/ASRL/senna/ -srl -verbose")
     labeler.expect('\[ready\]\r\n')
     return labeler
 
@@ -59,7 +66,6 @@ def getParseTree(parser, sentence):
     Start the parser with Popen for this function
     """
     #stdoutdata, stderrdata = parser.communicate(sentence)
-    import sys
     print sentence
     parser.stdin.write(sentence + '\n')
     parser.stdin.flush()
@@ -72,6 +78,9 @@ def getParseTree(parser, sentence):
     #return Tree(parser.stdout.read())
 
 def sennaToRoleLabels(sennaOut, stem=True):
+    """
+    Don't use this one anymore
+    """
     verbExtractor = r"\n\s*.*?\s+([-a-zA-Z]*?)\s+"
     allSrlVerbs = []
     stemmer = PorterStemmer()
@@ -85,44 +94,99 @@ def sennaToRoleLabels(sennaOut, stem=True):
     print allSrlVerbs 
     return allSrlVerbs
 
-def getRoleLabels(senna, sentence):
-    print sentence
-    senna.sendline(sentence)
-    senna.expect('\r\n\r\n')
-    return sennaToRoleLabels(senna.before)
+def getRoleLabelsPexpect(senna, sentence):
+    """
+    Don't use this one anymore
+    """
+    #print sentence
+    def cleanSentence(sentence):
+        toBeRemoved = r'[^a-zA-Z0-9,\.\s]'
+        ret = re.sub(toBeRemoved, '', sentence)
+        ret = ret.strip()
+        ret = ret.replace('\n',' ')
+        #print 'clean sentence'
+        #print ret
+        return ret
+    sentence = cleanSentence(sentence)
+    if len(sentence) > 10 and '\n' not in sentence:
+        senna.sendline(sentence)
+        time.sleep(2)
+        senna.expect('\r\n\r\n')
+        time.sleep(2)
+        return sennaToRoleLabels(senna.before)
+    else:
+        print 'Not doing anything.'
+        return []
 
-def srlCount(labeler, cases):
+def getRoleLabels(stem=True):
+    def cleanSennaOut(sennaRow):
+        if '' in sennaRow:
+            while '' in sennaRow:
+                sennaRow.remove('')
+        return sennaRow
+    ret = []
+    stemmer = PorterStemmer()
+    with open('tmp/sennaOut.txt', 'r') as text:
+        line = None
+        while line != '':
+            line = text.readline()
+            sennaRow = re.split('\s+', line)
+            sennaRow = cleanSennaOut(sennaRow)
+            if len(sennaRow) < 2:
+                continue
+            #print sennaRow
+            word = sennaRow[0]
+            verb = sennaRow[1]
+            if verb != '-':
+                if stem:
+                    ret.append(stemmer.stem(verb))
+                else:
+                    ret.append(verb)
+    return ret
+
+def srlCount(cases):
     from collections import Counter
+    def writeCase(tokens):
+        with open('tmp/sennaIn.txt', 'w') as out:
+            for token in tokens:
+                out.write(token + '\n')
     srlCount = Counter()
     trainer = nltk.tokenize.punkt.PunktSentenceTokenizer()
     trainer.train("GoogleCases.txt")
     for case in cases:
+        if type(case) != type(''):
+            print "Something went wrong - the case isn't a string"
+            continue
         tokens = trainer.tokenize(case)
-        for token in tokens:
-            roleLabels = getRoleLabels(labeler, token)
-            for role in roleLabels:
-                srlCount[role] = srlCount[role] + 1
+        writeCase(tokens)
+        start30SecProcess("./lib/ASRL/senna/senna -path lib/ASRL/senna/ -srl < tmp/sennaIn.txt > tmp/sennaOut.txt")
+        roleLabels = getRoleLabels()
+        for role in roleLabels:
+            srlCount[role] = srlCount[role] + 1
     return srlCount
 
 if __name__ == "__main__":
     import data
     testSet = [u'The parties agreed that the wife would receive the first $214,000 from the sale of the family residence, receipt of which would not constitute a change of circumstances for modification of spousal support.', "It makes no more sense to reduce wife's spousal support because she received her rightful share of the community property than it would to increase wife's spousal support because husband received his rightful share of the community property.", 'In Rabkin, the contested income took the form of $1,800 mortgage payments derived from the sale of the family residence which "constituted the single major asset awarded to wife as her one-half share of the community property."', "The court noted that, in any event, because the parties' agreement expressly provided that the sale of the residence would not constitute a change in circumstances justifying a reduction in support, the trial court's reliance on the sale in ordering a reduction was improper.", "We have concluded that none of these factors furnished a proper basis for the trial court's $250 per month reduction in wife's permanent spousal support.", 'The agreement provided for spousal support amounts established in contemplation of a prompt sale of the family residence.', 'The parties may also agree, as part of a support provision, that specified occurrences will or will not constitute the requisite change of circumstances to allow subsequent modification of that provision.', 'The trial court then reduced her spousal support based in part on the income she was receiving from the note.']
     #testSet = ['I walk the dog.']
-    labeler = startSRL()
+    #labeler = startSRL()
+    """
     for test in testSet:
         getRoleLabels(labeler, test)
     """
+    #"""
     nodes = otg.getNodes('data/spousal_support.txt')
     #for node in nodes:
     cites, titles = data.getGoogleCites(nodes[0].get('name'))
     titles = set(titles)
     cases = []
     for title in titles:
-        cases.append(data.getGoogleCase(title))
-    print srlCount(labeler, cases)
-    """
-    """parser = startProcess("java -Xmx1024m -jar lib/berkeleyParser.jar -gr lib/eng_sm6.gr")
+        case = data.getGoogleCase(title)
+        cases.append(case)
+    print srlCount(cases)
+    #"""
+    """parser = startProcessPopen("java -Xmx1024m -jar lib/berkeleyParser.jar -gr lib/eng_sm6.gr")
     for test in testSet:
         print getParseTree(parser, test)
     print getParseTree(parser, testSet[0])"""
-    labeler.close()
+    #labeler.close()
